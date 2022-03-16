65c38d7bc2d8d3aa34f8c5c4e1f5955708e50f1b
hemalatha <hema_26@hotmail.com>
2022-03-15 21:25:56 -0400

    Updated all the files from Valentina

--- js/TaskManager.js
+++ js/TaskManager.js
@@ -1,3 +1,22 @@
+
+const createTaskHtml = (name, description, assignedTo, dueDate, Status) => {
+  const taskHtml = `
+               <h4 class="card-title">Task Schedule</h4>
+               <ul id ="listelement">
+               <li class ="display">${name}</li>
+               <li class ="display">${description}</li>
+               <li class ="display">${assignedTo}</li>
+               <li class ="display">${dueDate}</li>
+               <li class ="display">${Status}</li>
+               <a class="btn btn-primary" href="Work-in-Progress.html" role="button" id ="button2">Work In Progress</a>
+               <button id ="button1" class="btn btn-primary" type="submit">Mark As Done</button>
+           </div>
+           `; 
+       //return `${name}, ${description}, ${assignedTo}, ${dueDate}, ${Status}`;
+       console.log(taskHtml);  
+   }
+    createTaskHtml();
+
 class TaskManager {
   constructor(currentId = 0) {
     this.tasks = [];
@@ -18,7 +37,26 @@ class TaskManager {
 
     console.log(this.tasks);
   }
+  render() {
+    const tasksHtmlList = [];
+    for (let i =0; i<this.tasks; i++){
+      const currentTask = this.tasks[i];
+      const date = new Date(this.dueDate);
+      const formattedDate = date.toString();
+      const taskHtml = createTaskHtml(currentTask.name, currentTask.description, currentTask.assignedTo,  currentTask.status, formattedDate);
+      tasksHtmlList.push(taskHtml);
+      
+    
+  }
+  const getval = document.getElementById("listelement")
+  const taskHtml1 = tasksHtmlList.join('\n');
+  getval.innerHtml =taskHtml1;
+//console.log(currentTask[i]);
 }
+
+ 
+
+
 
 // const newTask = new TaskManager();
 // newTask.addTask("study", "finalproject", "val", "Sunday");
@@ -26,5 +64,4 @@ class TaskManager {
 
 
 // newTask.addTask("study", "finalproject", "hema", "Sunday");
-
 
