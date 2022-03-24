0c6d24f69f7a4b6284938f01096ccfa7e1166286
Yohannes Maddie <jdemissie@yahoo.com>
2022-03-24 14:23:55 -0400

    merged

--- js/TaskManager.js
+++ js/TaskManager.js
@@ -116,46 +116,4 @@ class TaskManager {
     }
     this.tasks = newTasks;
   }
-  render() {
-    const tasksHtmlList = [];
-    for (let i =0; i<this.tasks; i++){
-      const currentTask = this.tasks[i];
-      const date = new Date(this.dueDate);
-      const formattedDate = date.toString();
-      const taskHtml = createTaskHtml(currentTask.name, currentTask.description, currentTask.assignedTo,  currentTask.status, formattedDate);
-      tasksHtmlList.push(taskHtml);
-      
-    
-  }
-  const getval = document.getElementById("listelement")
-  const taskHtml1 = tasksHtmlList.join('\n');
-  getval.innerHtml =taskHtml1;
-//console.log(currentTask[i]);
 }
-<<<<<<< HEAD
-
- const newTask = new TaskManager();
- newTask.addTask("study", "finalproject", "val", "Sunday");
-  console.log(newTask);
-newTask.render();
-
-// newTask.addTask("study", "finalproject", "hema", "Sunday");
-const createTaskHtml = (name, description, assignedTo, dueDate, Status) => {
-  const taskHtml = `
-               <h4 class="card-title">Task Schedule</h4>
-               <ul id ="listelement">
-               <li class ="display">${name}</li>
-               <li class ="display">${description}</li>
-               <li class ="display">${assignedTo}</li>
-               <li class ="display">${dueDate}</li>
-               <li class ="display">${Status}</li>
-               <a class="btn btn-primary" href="Work-in-Progress.html" role="button" id ="button2">Work In Progress</a>
-               <button id ="button1" class="btn btn-primary" type="submit">Mark As Done</button>
-           </div>
-           `; 
-       //return `${name}, ${description}, ${assignedTo}, ${dueDate}, ${Status}`;
-       console.log(taskHtml);  
-   }
-    createTaskHtml();
-=======
->>>>>>> a230b857754d57bb45815a192361ef82629011c7
